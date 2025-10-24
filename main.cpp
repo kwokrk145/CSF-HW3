375cfe2a0fd398e054d53bc927088825fef2957f
Ryan Kwok <kwokryan11@gmail.com>
2025-10-24 16:04:05 -0400

    Added struct definitions

--- main.cpp
+++ main.cpp
@@ -1,4 +1,25 @@
 #include <iostream>
+#include <vector>    
+#include <cstdint>   
+
+struct Block {
+    bool valid;
+    bool dirty;        
+    uint32_t tag;
+    uint64_t timestamp;
+};
+
+struct Set {
+    std::vector<Block> blocks;
+};
+
+struct Cache {
+    std::vector<Set> sets;
+};
+
+
+
+
 
 int main( int argc, char **argv ) {
   // TODO: implement
